71c8d60cef96fb61b4ddfe33f2a8bd98db64cc99
tamimhossain11 <amdtamim3@gmail.com>
2024-05-18 18:25:22 +0600

    implementing school finder

--- client/src/Components/structure/nav.js
+++ client/src/Components/structure/nav.js
@@ -4,13 +4,15 @@ import { Home } from "../../Pages/Home/Home";
 import { Login } from "../../Pages/Login";
 import { Portal1 } from "../../Pages/Portal1/Portal1";
 import { Portal2 } from "../../Pages/Portal2/Portal2";
+import { SchoolFinder } from "../../Pages/SchoolFinder/SchoolFinder";
 
 export const nav = (user) => {
   const nav = [
-    { path: "/", name: "Home", element: <Home />, isMenu: true, isPrivate: false },
-    { path: "/about", name: "About", element: <About />, isMenu: true, isPrivate: false },
-    { path: "/login", name: "Explore Portals", element: <Login />, isMenu: false, isPrivate: false },
-    { path: "/account", name: "Account", element: <Account />, isMenu: true, isPrivate: true },
+    { path:   "/",              name: "Home",             element: <Home />,         isMenu: true,   isPrivate: false },
+    { path:   "/about",         name: "About",            element: <About />,        isMenu: true,   isPrivate: false },
+    { path:   "/school_finder", name: "School Finder",    element: <SchoolFinder />, isMenu: true,   isPrivate: false },
+    { path:   "/login",         name: "Explore Portals",  element: <Login />,        isMenu: false,  isPrivate: false },
+    { path:   "/account",       name: "Account",          element: <Account />,      isMenu: true,   isPrivate: true },
   ];
 
   if (user.isAuthenticated) {
---
+++ client/src/Pages/SchoolFinder/SchoolFinder.js
@@ -0,0 +1,9 @@
+export const SchoolFinder = () => {
+
+    return (
+         <div className="page">
+              <h2>School Finder Search</h2>
+              <p>This is the text for the about page</p>
+         </div>
+    )
+}
